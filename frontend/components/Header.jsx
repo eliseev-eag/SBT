0751fdb6fe8ea6db9745d6b807a17da28e4046ee
eliseev-eag <eliseev-eag@yandex.ru>
2018-04-29 23:14:04 +0700

    Header изменен на "схлопывающийся" список

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,33 +1,43 @@
 import * as React from 'react';
-import { Nav, NavItem } from 'react-bootstrap';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export class Header extends React.Component {
     render() {
         return (
-            <Nav bsStyle="pills">
-                <NavItem
-                    componentClass={Link}
-                    href="/list"
-                    to="/list"
-                    active={location.pathname === '/list'}>
-                    Список сотрудников
-                </NavItem>
-                <NavItem
-                    componentClass={Link}
-                    href="/add"
-                    to="/add"
-                    active={location.pathname === '/add'}>
-                    Добавить запись
-                </NavItem>
-                <NavItem
-                    componentClass={Link}
-                    href="/settings"
-                    to="/settings"
-                    active={location.pathname === '/settings'}>
-                    Настройки
-                </NavItem>
-            </Nav>
+            <Navbar fluid>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        SBT
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem
+                            componentClass={Link}
+                            href="/list"
+                            to="/list"
+                            active={location.pathname === '/list'}>
+                            Список сотрудников
+                        </NavItem>
+                        <NavItem
+                            componentClass={Link}
+                            href="/add"
+                            to="/add"
+                            active={location.pathname === '/add'}>
+                            Добавить запись
+                        </NavItem>
+                        <NavItem
+                            componentClass={Link}
+                            href="/settings"
+                            to="/settings"
+                            active={location.pathname === '/settings'}>
+                            Настройки
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
